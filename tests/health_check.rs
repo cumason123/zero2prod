75b14cbbae1a0a867e876d0451127dd9538026b2
Curtis Mason <curtismason@Curtiss-Air.fios-router.home>
2021-08-22 18:24:46 -0400

    integration test health_check_works

---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+//! tests/health_check.rs
+//!
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    //
+    // New dev dependency - let's add tokio to the party with // `cargo add tokio --dev --vers 1`
+    let _ = tokio::spawn(server);
+}
+
+#[actix_rt::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
